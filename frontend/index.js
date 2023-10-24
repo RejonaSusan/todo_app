3350691fa9e0b3ef46e306bcadf81416bf47bde4
Rejona <rejwork0403@gmail.com>
2023-10-24 13:04:17 +0530

    Merge remote changes

--- frontend/index.js
+++ frontend/index.js
@@ -7,7 +7,6 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
-app.set("view engine", "ejs");
 
 app.get("/", (req,res)=>{
     var options = { weekday: 'short',  month: 'long', day: 'numeric' };
